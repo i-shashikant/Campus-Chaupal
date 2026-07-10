dd9ee625af519e0739bbf430d19bb10c0754238f
Shashikant <shashi873kant@gmail.com>
2026-07-10 19:39:04 +0530

    Dasboard update

---
+++ frontend/src/pages/student/Profile.vue
@@ -0,0 +1,36 @@
+<template>
+
+<DashboardLayout>
+
+    <div class="container-fluid">
+
+        <h2 class="mb-4">
+
+            My Profile
+
+        </h2>
+
+        <ProfileCard/>
+
+        <AcademicCard/>
+
+        <ProfessionalCard/>
+
+    </div>
+
+</DashboardLayout>
+
+</template>
+
+<script setup>
+
+import DashboardLayout from "@/layouts/DashboardLayout.vue";
+
+import ProfileCard from "@/components/student/ProfileCard.vue";
+
+import AcademicCard from "@/components/student/AcademicCard.vue";
+
+import ProfessionalCard from "@/components/student/ProfessionalCard.vue";
+
+</script>
+
