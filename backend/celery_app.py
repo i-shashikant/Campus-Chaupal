0578f51d8088f0cf8da6de31d014fd44e80f7431
Shashikant <shashi873kant@gmail.com>
2026-07-15 02:26:51 +0530

    Celery and redis integration

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -1,27 +1,24 @@
 from celery import Celery
 from app import create_app
-import tasks.reminders
 
 flask_app = create_app()
 
+celery = Celery(
+    flask_app.import_name,
+    broker=flask_app.config["CELERY_BROKER_URL"],
+    backend=flask_app.config["CELERY_RESULT_BACKEND"],
+)
 
-def make_celery(app):
-    celery = Celery(
-        app.import_name,
-        broker=app.config["CELERY_BROKER_URL"],
-        backend=app.config["CELERY_RESULT_BACKEND"],
-    )
-
-    celery.conf.update(app.config)
-
-    class ContextTask(celery.Task):
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return self.run(*args, **kwargs)
-
-    celery.Task = ContextTask
-    return celery
+celery.conf.update(flask_app.config)
 
 
-celery = make_celery(flask_app)
-celery.autodiscover_tasks(["tasks"])
+class ContextTask(celery.Task):
+    def __call__(self, *args, **kwargs):
+        with flask_app.app_context():
+            return self.run(*args, **kwargs)
+
+
+celery.Task = ContextTask
+
+# Explicitly import task modules
+import tasks.reminders
